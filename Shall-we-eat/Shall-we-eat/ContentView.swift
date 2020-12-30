4f011fd959dfb52e6e5c0a5ee544235bdcbbc803
Mariia Zhurina <mariia.zhurina@gmail.com>
2020-12-30 17:05:13 +0100

    Created project

---
+++ Shall-we-eat/Shall-we-eat/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shall-we-eat
+//
+//  Created by Mariia Zhurina on 30/12/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shall-we-eat/Shall-we-eat/Shall_we_eatApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shall_we_eatApp.swift
+//  Shall-we-eat
+//
+//  Created by Mariia Zhurina on 30/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct Shall_we_eatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
